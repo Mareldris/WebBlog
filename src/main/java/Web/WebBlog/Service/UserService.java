45f2d633959b123a34d13ffbce241f33aa017917
Sergey Mazunin <sergey.mazunin@mail.ru>
2020-09-29 10:27:48 +0500

    Стилизация часть 2, изменил header, добавил выпадающий список с именем пользователя, в котором добавил кнопку выхода, изменил видимость списка пользователей, теперь его видят только пользователи с ролью Admin, он находится в выпадющем списке

--- src/main/java/Web/WebBlog/Service/UserService.java
+++ src/main/java/Web/WebBlog/Service/UserService.java
@@ -22,3 +22,14 @@ public class UserService implements UserDetailsService {
         return userRepository.findByUsername(username);
     }
 }
+/*
+<div sec:authorize="isAuthenticated()">
+<form th:action="@{/logout}" method="post" sec:authorize="isAuthenticated()">
+<input type="hidden" th:name="${_csrf.parameterName}" th:value="${_csrf.token}"/>
+<div  sec:authentication="name">Unknown</div>
+<button class="btn btn-outline-success" type="submit">Выйти</button>
+</form>
+</div>
+<div sec:authorize="!isAuthenticated()">
+<a class="btn btn-outline-success" href="/login">Авторизоваться</a> <!-- Сделать Выход -->
+</div>*/
